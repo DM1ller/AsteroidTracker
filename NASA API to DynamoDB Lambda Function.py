9168a6b437c02686fb1a3f9a62004b3181125772
Dylan-Miller1 <134555676+Dylan-Miller1@users.noreply.github.com>
2023-05-27 16:30:23 -0700

    Update flow chart. Updated NASA API to DynamoDB variable names and spacing

--- NASA API to DynamoDB Lambda Function.py
+++ NASA API to DynamoDB Lambda Function.py
@@ -4,33 +4,23 @@ import boto3
 
 client = boto3.client('dynamodb')
 
-
 def get_asteroids():
     response = requests.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-05-11&end_date=2023-05-11&api_key=7m6UK6aibw2jHOAvKOAgF6T9AJvMKK21zduo4ZhY')
     data = response.json()
-    shortData = data['near_earth_objects']['2023-05-11']
+    short_data = data['near_earth_objects']['2023-05-11']
 
-    for asteroid in range(len(shortData)):
-        asteroid_id = shortData[asteroid]['id']
-        asteroid_name = shortData[asteroid]['name']
-        hazardous = shortData[asteroid]['is_potentially_hazardous_asteroid']
-        minSize = shortData[asteroid]['estimated_diameter']['meters']['estimated_diameter_min']
-        maxSize = shortData[asteroid]['estimated_diameter']['meters']['estimated_diameter_max']
-        averageSize = str((maxSize + minSize)//2)
-        close_approach_date = shortData[asteroid]['close_approach_data'][0]['close_approach_date']
-        orbiting_body = shortData[asteroid]['close_approach_data'][0]['orbiting_body']
-        relative_velocity = str(int(float(shortData[asteroid]['close_approach_data'][0]['relative_velocity']['kilometers_per_hour'])//1))
+    for asteroid in range(len(short_data)):
+        asteroid_id = short_data[asteroid]['id']
+        asteroid_name = short_data[asteroid]['name']
+        hazardous = short_data[asteroid]['is_potentially_hazardous_asteroid']
+        min_size = short_data[asteroid]['estimated_diameter']['meters']['estimated_diameter_min']
+        max_size = short_data[asteroid]['estimated_diameter']['meters']['estimated_diameter_max']
+        average_size = str((max_size + min_size)//2)
+        close_approach_date = short_data[asteroid]['close_approach_data'][0]['close_approach_date']
+        orbiting_body = short_data[asteroid]['close_approach_data'][0]['orbiting_body']
+        relative_velocity = str(int(float(short_data[asteroid]['close_approach_data'][0]['relative_velocity']['kilometers_per_hour'])//1))
         
-        
-
-        print (f'Asteroid ID: {asteroid_id} \nSize:{averageSize}\nIs Hazardous:{hazardous}\n Close Approach Date:{close_approach_date} \n  \n')
-        print(type(close_approach_date))
-        
-        data = client.put_item(TableName = 'Asteroids_DynamoDB', Item={'ID':{'S':asteroid_id}, 'Name':{'S':asteroid_name}, 'Size (Meters)': {'N':averageSize}, 'Relative Velocity (KPH)': {'S':relative_velocity}, 'Orbiting Body':{'S':orbiting_body}, 'Is Hazardous': {'BOOL':hazardous}})
-
-   
-   
-
+        client.put_item(TableName = 'Asteroids_DynamoDB', Item={'ID':{'S':asteroid_id}, 'Name':{'S':asteroid_name}, 'Size (Meters)': {'N':average_size}, 'Relative Velocity (KPH)': {'S':relative_velocity}, 'Orbiting Body':{'S':orbiting_body}, 'Is Hazardous': {'BOOL':hazardous}})
 
 def lambda_handler(event, context):
     get_asteroids()
