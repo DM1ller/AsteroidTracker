93c499e51a21b805662ca77039bf77d1546eb852
Dylan-Miller1 <134555676+Dylan-Miller1@users.noreply.github.com>
2023-05-27 16:40:38 -0700

    Updated line spacing in "Get Data DynamoDB Lambda"

--- Get Data From DynamoDB Lambda.py
+++ Get Data From DynamoDB Lambda.py
@@ -9,15 +9,13 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Headers" : "Content-Type",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-            
         },
         'body': json.dumps(data)
     }
     return response
     
-    
-    
 def get_data_dynamodb():    
     dynamodb = boto3.client('dynamodb')
     response = dynamodb.scan(TableName='Asteroids_DynamoDB')
     return response['Items']
+    
